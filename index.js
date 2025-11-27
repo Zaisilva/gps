e4bff809fb92d01a5a24aa9943e2e0e2d6546b4c
Zaisilva <zagarsil.10@gmail.com>
2025-11-27 00:47:51 -0600

    Test auto deploy v3

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ app.use(express.json());
 // Rutas
 app.get('/', (req, res) => {
   res.json({ 
-    message: '¡Hola! Backend funcionando correctamente esto es un cambio, gracias',
-    timestamp: new Date().toISOString(),
+    message: '¡Hola! Backend funcionando correctamente  DESPLEGADO AUTOMÁTICAMENTE v3.0',    timestamp: new Date().toISOString(),
     version: '1.0.0'
   });
 });
